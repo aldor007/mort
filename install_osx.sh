a6cfcb39356d8c3dc39caaece9b482700b7627b1
Marcin Kaciuba <aldor007@gmail.com>
2017-09-22 08:00:02 +0200

    added file type

--- install_osx.sh
+++ install_osx.sh
@@ -3,3 +3,5 @@ go get gopkg.in/yaml.v2
 brew tap homebrew/science
 brew install vips --with-cfitsio --with-imagemagick --with-openexr --with-openslide --with-webp
 go get -u gopkg.in/h2non/bimg.v1
+go get -u github.com/graymeta/stow
+go get gopkg.in/h2non/filetype.v1
--- src/imgserver/processor.go
+++ src/imgserver/processor.go
@@ -7,30 +7,49 @@ import (
 	"imgserver/object"
 	"imgserver/response"
 	"gopkg.in/h2non/bimg.v1"
+	"gopkg.in/h2non/filetype.v1"
+	"imgserver/engine"
+	"net"
+	"imgserver/transforms"
 )
 
-func Process(obj *object.FileObject) (*response.Response) {
-	// check if parent exists
+func Process(obj *object.FileObject) (response.Response) {
+	var parent response.Response
 	if obj.HasParent() {
-		// TODO head method
-		parent := storage.Get(obj.GetParent())
-		if parent.StatusCode == 404 {
-			return parent
+		parent = storage.Get(obj.GetParent())
+	}
+
+	if len(obj.Transforms) == 0  &&  obj.Params == nil {
+		return storage.Get(obj)
+	}
+
+	if parent.StatusCode == 404 {
+		return parent
+	}
+
+	if len(parent.Body) > 261 && filetype.IsImage(parent.Body[:261]) {
+		return processImage(parent.Body, obj.Transforms, obj.Params)
+	}
+
+	return storage.Get(obj)
+}
+
+
+func processImage(body []byte, transforms transforms.Base, param transforms.Param) (response.Response)
+	var result byte[]
+	engine := engine.NewImageEngine(body, &resutl}
+	for trans := range transforms {
+		err := engine.Process(trans)
+		if err != nil {
+			log.Fatal("Unable to process %s err %s", trans.Name, err)
+			return response.WrapWithError
 		}
 	}
 
-	res := storage.Get(obj)
+	engine.Param(param)
+
+	result := engine.Result()
 
 
 
-	return res
-
-}
-
-func processResponse(obj *object.FileObject, res *response.Response) (*response.Response) {
-	processImage(obj, res)
-}
-
-func processImage(obj   *object.FileObject, res *response.Response) {
-	bimg.
 }
