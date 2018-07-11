e7f0552f48e5d88dd07ee2003ecc9aab479fb8c0
Marcin Kaciuba <aldor007@gmail.com>
2018-07-11 17:53:40 +0200

    Setup example

--- example/deploy-mort.sh
+++ example/deploy-mort.sh
@@ -4,14 +4,4 @@ docker build . -t mort
 docker ps | grep mort | awk '{print $1 }' | xargs docker stop
 docker ps -a | grep mort | awk '{print $1 }' | xargs docker rm
 rm -rf /var/run/mort/mort.sock
-docker run  --name mort -d  -p 127.0.0.1:8080:8080  -p 127.0.0.1:8081:8081 -v /var/mort/data/:/data/buckets -v /var/run/mort/:/var/run/mort mort
-counter=0
-while [ ! -f /var/run/mort/mort.sock ]; do
-    sleep 1
-    counter=$counter+1
-    if (( $counter > 10 )); then
-        setfacl -m m::rwx /var/run/mort/mort.sock
-        exit $?
-    fi
-    sleep 1
-done
+docker run  --name mort -d  -p "127.0.0.1:8080:8080"  -p "127.0.0.1:8081:8081" -v /var/mort/data/:/data/buckets -v /var/run/mort/:/var/run/mort mort
