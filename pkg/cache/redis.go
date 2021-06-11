9c7a7227a4e5882696b6f1bcedccf5e137dbd22d
Jarek Lisicki <jaroslaw.lisicki@gmail.com>
2021-06-11 09:21:19 +0200

    Response simplified and added improvements for S3

--- pkg/cache/redis.go
+++ pkg/cache/redis.go
@@ -2,14 +2,15 @@ package cache
 
 import (
 	"context"
+	"strings"
+	"time"
+
 	"github.com/aldor007/mort/pkg/monitoring"
 	"github.com/aldor007/mort/pkg/object"
 	"github.com/aldor007/mort/pkg/response"
 	redisCache "github.com/go-redis/cache/v8"
 	goRedis "github.com/go-redis/redis/v8"
 	"github.com/vmihailenco/msgpack"
-	"strings"
-	"time"
 )
 
 func parseAddress(addrs []string) map[string]string {
@@ -34,7 +35,7 @@ func NewRedis(redisAddress []string, clientConfig map[string]string) *RedisCache
 		Addrs: parseAddress(redisAddress),
 	})
 	cache := redisCache.New(&redisCache.Options{
-		Redis: ring,
+		Redis:      ring,
 		LocalCache: redisCache.NewTinyLFU(10, time.Minute),
 	})
 
@@ -71,14 +72,14 @@ func (c *RedisCache) getKey(obj *object.FileObject) string {
 // Set put response into cache
 func (c *RedisCache) Set(obj *object.FileObject, res *response.Response) error {
 	monitoring.Report().Inc("cache_ratio;status:set")
-	v ,err :=  msgpack.Marshal(res)
+	v, err := msgpack.Marshal(res)
 	if err != nil {
 		return err
 	}
 	item := redisCache.Item{
-		Key:      c.getKey(obj),
-		Value:   v,
-		TTL: time.Second * time.Duration(res.GetTTL()),
+		Key:   c.getKey(obj),
+		Value: v,
+		TTL:   time.Second * time.Duration(res.GetTTL()),
 	}
 	return c.client.Set(obj.Ctx, &item)
 }
