568632c356090c80667f8a95c74233cbcd08a8de
Marcin Kaciuba <marcin.kaciuba@gmail.com>
2022-03-19 19:56:07 +0100

    chore: update github integration action

--- scripts/prepare-for-tests.sh
+++ scripts/prepare-for-tests.sh
@@ -7,7 +7,6 @@ mkdir -p /tmp/mort-tests/remote/dir
 
 echo "test" > /tmp/mort-tests/local/file
 echo "test" > /tmp/mort-tests/remote/file
-wget https://mort.mkaciuba.com/assets/mkaciuba/main.c478fdbeab204c1059c6.css -O /tmp/mort-tests/local/main.css
 fallocate -l 1G /tmp/mort-tests/local/big.img
 
 cp -r pkg/processor/benchmark/local/* /tmp/mort-tests/local/
