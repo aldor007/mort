b1bbb6d658efe58010260bb7b51ab1c03a5ab4f1
Marcin Kaciuba <marcin.kaciuba@gmail.com>
2022-10-20 08:55:13 +0200

    fix: fix redis lock

--- pkg/lock/redis.go
+++ pkg/lock/redis.go
@@ -120,9 +120,11 @@ func (m *RedisLock) Lock(ctx context.Context, key string) (result LockResult, ok
 					select {
 					case <-ch:
 						m.NotifyAndRelease(ctx, key, nil)
+						return
 					case <-result.Cancel:
 						m.memoryLock.Release(ctx, key)
 						pubsub.Close()
+						return
 					case <-ctx.Done():
 						m.memoryLock.Release(ctx, key)
 						pubsub.Close()
