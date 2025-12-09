e3bba2c96d89adf0fa2fe6381bf997739ba31862
Marcin Kaciuba <marcin.kaciuba@gmail.com>
2025-12-09 10:58:29 +0100

    fix tests

--- pkg/helpers/helpers.go
+++ pkg/helpers/helpers.go
@@ -19,9 +19,6 @@ var client = &http.Client{
 		ResponseHeaderTimeout: 10 * time.Second,
 		ExpectContinueTimeout: 1 * time.Second,
 	},
-	CheckRedirect: func(req *http.Request, via []*http.Request) error {
-		return http.ErrUseLastResponse
-	},
 }
 
 // FetchObject download data from given URI
